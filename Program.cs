9e93abcbe3c3aa5da91d90a090aea7f29ef3d0d1
ImNSv <no.natalya.savina@gmail.com>
2023-07-26 22:51:05 +0400

    homework1

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");*/
+
+/* решена Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b){
+    Console.WriteLine($"{a} больше {b}");
+}
+else Console.WriteLine($"{b} больше {a}");*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+/* Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int d = int.Parse(Console.ReadLine()!);
+
+if(a > b && a > d) Console.WriteLine($"{a} больше всех");
+if(b > a && b > d) Console.WriteLine($"{b} больше всех");
+else Console.WriteLine($"{d} больше всех");*/
+
+/* решена адача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*Console.Write("Введите первое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num = number%2;  
+
+if(num == 0) Console.WriteLine($"{number} да");
+else Console.WriteLine($"{number} нет");*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+
+/*Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int x = number%2;
+
+while(number > 1 & x==0) {
+    Console.Write($"{number}");
+    number++;
+}*/
